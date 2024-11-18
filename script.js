20e8d45eaf92e3d511b00370e0122a78dc57b643
gokulgramcattlebreedingfarmratona <gokulgramcattlebreedingfarm@gmail.com>
2024-11-18 20:29:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
 function login(){
-                Let username = document.getElementById("username").value;
-
-                Let password2 = document.getElementById("password_user").value;
-
-                Let website_username = "ggcbfratona21";
-
-                Let website_password = "Ratona@2024";
-
-                if(username == website_username && password2 == website_password)
-                {
-                    window.location="indexuser.html";
-                }
-                else if(username == "" && password2 == "")
-                {
-                    alert("please type username and password");
-                }
-                else if(username == website_username && password2 !== website_password)
-                {
-                    document.getElementById("username").style.border="1px solid green";
-                    document.getElementById("password_user").style.border="1px solid red";
-                }
-                else if(username !== website_username && password2 == website_password)
-                {
-                    document.getElementById("username").style.border="1px solid red";
-                    document.getElementById("password_user").style.border="1px solid green";
-                }
+  var uname=document.getElementById('username').value;
+  var pass=document.getElementById('password').value;
+  if(uname=="ggcbfratona21" && password=="Ratona@2024")
+  {
+    location.assign("https://gokulgramcattlebreedingfarmratona.github.io/gggcbf/indexuser.html");
+  }
+  else
+  {
+    window.alert("login failed");
+  }
 }
